2b730f1b533e011d0704ff69b6009d5429de2630
Ramon Chancay <rchancay@eluniverso.com>
2015-02-09 17:08:24 -0500

    add compress,copy,build,server-dist 'task' edit index.html, folder structure stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var gulp = require('gulp'),
+var gulp    = require('gulp'),
 	connect = require('gulp-connect'),
-	stylus = require('gulp-stylus'),
-	nib = require('nib'),
-	jshint = require('gulp-jshint'),
+	stylus  = require('gulp-stylus'),
+	nib     = require('nib'),
+	jshint  = require('gulp-jshint'),
 	stylish = require('jshint-stylish'),
-	historyApiFallback = require('connect-history-api-fallback'),
-	inject = require('gulp-inject'),
-	wiredep = require('wiredep').stream;;
+	inject  = require('gulp-inject'),
+	wiredep = require('wiredep').stream,
+	historyApiFallback = require('connect-history-api-fallback');
 
 gulp.task('inject', function() {
 	var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css']);
@@ -48,12 +48,14 @@ gulp.task('jshint', function() {
 		.pipe(jshint.reporter('fail'));
 });
 
+
 gulp.task('css', function() {
-	gulp.src('./app/stylesheets/main.styl')
+	gulp.src('./app/stylus/**/*.styl')
 		.pipe(stylus({ use: nib() }))
 		.pipe(gulp.dest('./app/stylesheets'))
 		.pipe(connect.reload());
-	});
+});
+
 
 gulp.task('html', function() {
 	gulp.src('./app/**/*.html')
@@ -62,9 +64,43 @@ gulp.task('html', function() {
 
 gulp.task('watch', function() {
 	gulp.watch(['./app/**/*.html'],['html']);
-	gulp.watch(['./app/stylesheets/**/*.styl'], ['css', 'inject']);
+	gulp.watch(['./app/stylus/**/*.styl'], ['css', 'inject']);
 	gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
 	gulp.watch(['./bower.json'], ['wiredep']);
 });
 	
 gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+
+var gulpif     = require('gulp-if'),
+ 	minifyCss  = require('gulp-minify-css'),
+ 	useref     = require('gulp-useref'),
+ 	uglify     = require('gulp-uglify');
+
+gulp.task('compress',function(){
+	gulp.src('./app/index.html')
+		.pipe(useref.assets())
+		.pipe(gulpif('*.js',uglify({mangle: false})))
+		.pipe(gulpif('*.css',minifyCss()))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy',function(){
+	gulp.src('./app/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build',['compress','copy']);
+
+gulp.task('server-dist',function(){
+	connect.server({
+		root: './dist',
+		hostname: '0.0.0.0',
+		port: 8080,
+		livereload: true,
+		middleware: function(connect, opt) {
+			return [ historyApiFallback ];
+		}
+	});
+});
+
