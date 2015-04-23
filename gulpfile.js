b9dfbd1f8b34c8e66502b5756eb90eb6f899aff6
Ramon Chancay <rchancay@eluniverso.com>
2015-04-23 15:48:29 -0500

    refactorizar para npm

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,15 @@ var gulp    = require('gulp'),
 	wiredep = require('wiredep').stream,
 	historyApiFallback = require('connect-history-api-fallback'),
 	rupture = require('rupture'),
-	Pageres = require('pageres');
+	Pageres = require('pageres'),
+	util = require('gulp-util'),
+	notify = require('gulp-notify');
+
+
+var params = {
+	'folderApp' : './app',
+	'folderDist': './dist'
+};
 
 
 var devices = ['1024x768','480x320','1800x968','iphone5s'];
@@ -18,7 +26,7 @@ var devices = ['1024x768','480x320','1800x968','iphone5s'];
 gulp.task('shot', function(){
 	var pageres = new Pageres({delay:4})
 		.src('ramonchancay.com',devices,{crop:false})
-		.dest('./app/captures/');
+		.dest(params.folderApp+'captures/');
 
 	pageres.run(function(err){
 		if(err){
@@ -29,7 +37,7 @@ gulp.task('shot', function(){
 });
 
 gulp.task('inject', function() {
-	var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css']);
+	var sources = gulp.src([params.folderApp+'scripts/**/*.js',params.folderApp+'stylesheets/**/*.css']);
 	return gulp.src('index.html', {cwd: './app'})
 		.pipe(inject(sources, {
 				read: false,
@@ -39,14 +47,15 @@ gulp.task('inject', function() {
 });
 
 gulp.task('wiredep', function () {
-	gulp.src('./app/index.html')
+	/*gulp.src(params.folderApp+'index.html')
 	.pipe(wiredep({
-		directory: './app/lib'
+		directory: params.folderApp+'lib'
 	}))
-	.pipe(gulp.dest('./app'));
+	.pipe(gulp.dest('./app'));*/
 });
 
 gulp.task('server', function() {
+
 	connect.server({
 		root: './app',
 		hostname: '0.0.0.0',
@@ -60,7 +69,7 @@ gulp.task('server', function() {
 
 
 gulp.task('jshint', function() {
-	return gulp.src('./app/scripts/**/*.js')
+	return gulp.src(params.folderApp+'scripts/**/*.js')
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(jshint.reporter('fail'));
@@ -68,26 +77,40 @@ gulp.task('jshint', function() {
 
 
 gulp.task('css', function() {
-	gulp.src('./app/stylus/**/main.styl')
+	gulp.src(params.folderApp+'stylus/**/main.styl')
 		.pipe(stylus({ use: [nib(),rupture()] }))
-		.pipe(gulp.dest('./app/stylesheets'))
+		.pipe(gulp.dest(params.folderApp+'stylesheets'))
 		.pipe(connect.reload());
 });
 
 
 gulp.task('html', function() {
-	gulp.src('./app/**/*.html')
+	gulp.src(params.folderApp+'**/*.html')
 		.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-	gulp.watch(['./app/**/*.html'],['html']);
-	gulp.watch(['./app/stylus/**/*.styl'], ['css', 'inject']);
-	gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
-	gulp.watch(['./bower.json'], ['wiredep']);
+	gulp.watch([params.folderApp+'**/*.html'],['html']);
+	gulp.watch([params.folderApp+'stylus/**/*.styl'], ['css', 'inject']);
+	gulp.watch([params.folderApp+'scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+	//gulp.watch(['./bower.json'], ['wiredep']);
 });
 	
-gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+gulp.task('default',function(){
+	
+	try{
+		if(typeof util.env.folder !== undefined && typeof util.env.dist !== undefined){
+			params.folderApp = util.env.folder;
+			params.folderDist = util.env.dist;
+			gulp.run('launch');
+		}
+	}catch(e){
+		gulp.run('launch');
+	}
+});	
+
+
+gulp.task('launch', ['server', 'inject', 'wiredep', 'watch']);
 
 var gulpif     = require('gulp-if'),
 	minifyCss  = require('gulp-minify-css'),
@@ -101,29 +124,29 @@ gulp.task('compress',function() {
 	var assets = useref.assets(),
 	opts = { comments:false };
 
-	gulp.src('./app/*.html')
+	gulp.src(params.folderApp+'*.html')
 		.pipe(assets)
 		.pipe(gulpif('*.css',minifyCss({keepSpecialComments : 0})))
 		.pipe(gulpif('*.js', uglify({mangle: false })))
 		.pipe(assets.restore())
 		.pipe(useref())
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest(params.folderDist+''));
 	
 	setTimeout(function(){
-		gulp.src('./build/*.html')
+		gulp.src(params.folderDist+'/*.html')
 			.pipe(minifyHTML(opts))
-			.pipe(gulp.dest('./build'));
+			.pipe(gulp.dest(params.folderDist+''));
 	},1000);
 });
 
 gulp.task('uncss', function() {
 	setTimeout(function(){
-		gulp.src('./build/stylesheets/main.min.css')
+		gulp.src(params.folderDist+'/stylesheets/main.min.css')
 			.pipe(uncss({
-				html: ['./app/index.html']
+				html: [params.folderApp+'index.html']
 			}))
 			.pipe(minifyCss({keepSpecialComments : 0}))
-			.pipe(gulp.dest('./build/stylesheets'));
+			.pipe(gulp.dest(params.folderDist+'/stylesheets'));
 	},2000);
 });
 
@@ -131,7 +154,7 @@ gulp.task('build',['compress','uncss']);
 
 gulp.task('server-build',function(){
 	connect.server({
-		root: './build',
+		root: params.folderDist+'',
 		hostname: '0.0.0.0',
 		port: 8080,
 		livereload: true,
